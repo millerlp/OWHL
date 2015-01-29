8ed5e21e1502635748a8a4eb8f89eabe016203ad
Luke Miller <millerlp@gmail.com>
2015-01-28 16:34:11 -0800

    Fixing links to RTClib library

--- OWHL/OWHL.ino
+++ OWHL/OWHL.ino
@@ -81,8 +81,8 @@
  */
 #include <SPI.h> // stock Arduino library
 #include <Wire.h> // stock Arduino library
-#include <RTClib.h> // https://github.com/darkazazeal/rtclib
-#include <RTC_DS3231.h> // https://github.com/darkazazeal/rtclib
+#include <RTClib.h> // https://github.com/mizraith/RTClib
+#include <RTC_DS3231.h> // https://github.com/mizraith/RTClib
 #include <MS5803_14.h> // https://github.com/millerlp/MS5803_14
 #include <SdFat.h> // https://github.com/greiman/SdFat
 
